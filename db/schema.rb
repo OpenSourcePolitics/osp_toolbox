3e13f0b4259f4a9f50104d663fb5d9ce05c6cf36
QuentinLp <quentin.le-port@ensg.eu>
2021-08-06 12:18:41 +0200

    Update db structure with archives

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_04_161111) do
+ActiveRecord::Schema.define(version: 2021_08_06_092424) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,13 @@ ActiveRecord::Schema.define(version: 2021_08_04_161111) do
     t.integer "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "archives", force: :cascade do |t|
+    t.integer "document_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["document_id"], name: "index_archives_on_document_id"
   end
 
   create_table "documents", force: :cascade do |t|
@@ -71,6 +78,7 @@ ActiveRecord::Schema.define(version: 2021_08_04_161111) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "archives", "documents"
   add_foreign_key "documents", "users"
   add_foreign_key "input_files", "documents"
 end
