64f8538f3191d745530f7d282e355e83465173c7
QuentinLp <quentin.le-port@ensg.eu>
2021-08-07 15:39:45 +0200

    Change default flask server url

--- lib/comments_mapping_request.rb
+++ lib/comments_mapping_request.rb
@@ -3,7 +3,7 @@ require 'net/http'
 require 'uri'
 
 module CommentsMappingRequest
-  def self.send_post_request(content, url = URI("http://0.0.0.0:5000/?sorting_attribute=comments"))
+  def self.send_post_request(content, url = URI("http://127.0.0.1:5000/?sorting_attribute=supports"))
     http = Net::HTTP.new(url.host, url.port)
     request = Net::HTTP::Post.new(url)
     request["Content-Type"] = "application/json"
