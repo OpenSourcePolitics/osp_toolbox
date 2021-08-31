d7f9c3ec8a59e7021bb8cdf11aedbe307f0170ee
Armand Fardeau <armandfardeau@users.noreply.github.com>
2021-08-31 11:49:45 +0200

    Add production settings (#8)

--- Gemfile
+++ Gemfile
@@ -58,6 +58,7 @@ end
 
 group :production do
   gem 'pg'
+  gem 'aws-sdk-s3', require: false
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.active_storage.service = :scaleway
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
