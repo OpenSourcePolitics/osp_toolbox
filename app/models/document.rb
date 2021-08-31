40da69c5f89b9cb54973120f78fb17b5e6667118
Armand Fardeau <armandfardeau@users.noreply.github.com>
2021-08-31 14:41:02 +0200

    Add upload button (#11)

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,10 +2,4 @@ class Document < ApplicationRecord
   belongs_to :user
   has_many :input_files, dependent: :destroy
   has_one :archive, dependent: :destroy
-
-  after_save :process_document, if: ->(document) { document.ready? }
-
-  def process_document
-    ProcessDocumentJob.perform_later(self)
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :documents do
     resources :input_files, except: [:index]
+    match '/process_archive', to: 'documents#process_archive', via: :post
   end
+
   devise_for :users
   root to: "pages#home"
 end
