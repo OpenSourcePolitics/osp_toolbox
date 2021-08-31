76340d7321f40b7a88f02ddaf766fe77996ed9e1
Armand Fardeau <fardeauarmand@gmail.com>
2021-08-31 12:15:59 +0200

    Fix duplicate key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,8 +3,6 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.active_storage.service = :scaleway
-
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -39,8 +37,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :scaleway
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
