956779a0a3f21b830f77380217d3361ed6a24a4f
Armand Fardeau <fardeauarmand@gmail.com>
2021-09-01 23:58:09 +0200

    Change callback order

--- app/models/preprocessing.rb
+++ app/models/preprocessing.rb
@@ -2,10 +2,8 @@
 
 # Model for preprocessing file
 class Preprocessing < ApplicationRecord
-  has_one_attached :file
-
   after_commit :preprocess_data, if: -> { sent_to_preprocessing_at.nil? }
-
+  has_one_attached :file
   validates :file, attached: true, content_type: %w[text/csv application/vnd.ms-excel]
 
   def preprocess_data
