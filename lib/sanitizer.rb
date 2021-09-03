78264eda2bc7418f4ec0f2054755c98a1038155c
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2021-09-03 18:41:09 +0200

    Add sanitizer module for filenames

---
+++ lib/sanitizer.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# This module provides methods for sanitizing
+module Sanitizer
+  # filename: Returns a parameterize filename or static filename with timestamp if 'filename' parameter is nil or empty
+  # params: filename : String
+  # returns: String
+  def self.filename(filename)
+    return "#{timestamp}_without_name" if filename.blank?
+
+    filename.parameterize(separator: "_")
+  end
+
+  private
+
+  # timestamps: Returns a timestamp
+  # returns: Integer
+  def timestamp
+    DateTime.now.to_i
+  end
+end
