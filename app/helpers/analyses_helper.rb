31e7bef22a6fcb56238898be52b3e534fb2bc364
Armand Fardeau <fardeauarmand@gmail.com>
2021-09-15 22:54:07 +0200

    Fix missing method

--- app/helpers/analyses_helper.rb
+++ app/helpers/analyses_helper.rb
@@ -1,13 +1,13 @@
 module AnalysesHelper
   def available_analyses_typename
-    Analysis.available_analyses
+    Analysis::AVAILABLE_ANALYSES
   end
 
   def available_analyses_typename_for_select
-    available_analyses_typename.map(&:to_s).map { |typename| [typename.titleize, typename] }
+    available_analyses_typename.map(&:to_s).map { |typename| [typename.capitalize, typename] }
   end
 
   def categories_for_select(processing)
-    processing.available_categories.map { |category| [category&.titleize, category] }
+    processing.available_categories.map { |category| [category&.capitalize, category] }
   end
 end
