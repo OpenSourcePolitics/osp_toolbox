760d748eef9aa7f69b420a3730dece0c1a3596c7
Armand Fardeau <fardeauarmand@gmail.com>
2021-09-16 11:32:39 +0200

    Use sentry only in production

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,8 @@
-Sentry.init do |config|
-  config.dsn = ENV["SENTRY_DSN"]
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+if Rails.env.production?
+  Sentry.init do |config|
+    config.dsn = ENV["SENTRY_DSN"]
+    config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
-  config.traces_sample_rate = 1.0
+    config.traces_sample_rate = 1.0
+  end
 end
