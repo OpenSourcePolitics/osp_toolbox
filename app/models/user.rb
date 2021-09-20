240b883dfb1ac2ef05053f824be74090e52cc19a
Armand Fardeau <fardeauarmand@gmail.com>
2021-09-20 19:08:51 +0200

    Add allowed domains list

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,19 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :documents, dependent: :destroy
+  before_save :check_email_address
 
   def nickname=(string)
     value = string.start_with?("@") ? string : "@#{string}"
 
     super(value)
+  end
+
+  def check_email_address
+    return if Rails.configuration.allowed_domains.empty?
+
+    if Rails.configuration.allowed_domains.none? { |domain| email.end_with?(domain) }
+      self.errors.add(:base, I18n.t("wrong_domain", scope: "user.validations", domains: Rails.configuration.allowed_domains.join(", ")))
+    end
   end
 end
