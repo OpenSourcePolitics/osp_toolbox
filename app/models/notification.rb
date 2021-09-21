ca26eb43f8ceb85b63d7a81262e0bc9bf12769ac
Armand Fardeau <fardeauarmand@gmail.com>
2021-09-21 23:14:23 +0200

    Add Analyses_data request tests

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,8 +2,14 @@
 
 # Notification class
 class Notification < ApplicationRecord
+  class << self
+    alias_method :register!, :create!
+  end
+
   def self.notify!(resource)
     notification = Notification.find_by(resource_class: resource.class.name, resource_id: resource.id)
+
+    return if notification.nil?
 
     send_notification(User.find(notification.target_id), resource.notification_message)
 
@@ -12,12 +18,8 @@ class Notification < ApplicationRecord
 
   def self.send_notification(user, message)
     url = URI(ENV["ROCKET_CHAT_URL"])
-    content = { text: I18n.t("notification", scope: "rocket_chat", user: user.nickname, message: message) }
+    content = {text: I18n.t("notification", scope: "rocket_chat", user: user.nickname, message: message)}
 
     RequestBuilder.send_post_request(content, url)
-  end
-
-  def self.register!(event_name:, resource_class:, resource_id:, target_id:)
-    create!(event_name: event_name, resource_class: resource_class, resource_id: resource_id, target_id: target_id)
   end
 end
---
+++ spec/requests/analyses_data_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Analyses_data", type: :request do
+  let(:analysis) { create(:analysis) }
+  let(:token) { "f84a8a0eb816bf8d11ca09bebdfb4001c35b56baa44fc228da4b8513056d98c3" }
+  let(:archive_file) do
+    Rack::Test::UploadedFile.new(Rails.root.join("db/seeds_data/wordclouds.png"))
+  end
+
+  describe "POST /store_analysis_data" do
+    before do
+      post "/analysis_data", params: {
+          token: token,
+          archive: archive_file,
+          analysis_id: analysis.id
+      }
+    end
+
+    it "returns a 200" do
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+      expect(response).to have_http_status(:ok)
+    end
+
+    context "when token does not match" do
+      let(:token) { "dummy" }
+
+      it "returns a 401" do
+        expect(response.content_type).to eq("application/json; charset=utf-8")
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+  end
+end
