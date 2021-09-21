7a11975e4a94b52767264d463e5fc891c5bf4890
Armand Fardeau <fardeauarmand@gmail.com>
2021-09-21 15:33:13 +0200

    Revert "Remove maintain schema"
    This reverts commit 6050c04efce7a5f112984c4e28b920546e8e7910.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,12 @@ require "rspec/rails"
 require "support/factory_bot"
 require "support/capybara"
 
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
+end
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
