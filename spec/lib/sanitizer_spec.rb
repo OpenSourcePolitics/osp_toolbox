1db3830c1d2845be9a322741396c191728d3a264
Armand Fardeau <armandfardeau@users.noreply.github.com>
2021-09-23 01:28:16 +0200

    Implement sanitizer lib tests (#59)
    * Implement sanitizer lib tests
    
    * Fix offenses

--- spec/lib/sanitizer_spec.rb
+++ spec/lib/sanitizer_spec.rb
@@ -3,5 +3,24 @@
 require "rails_helper"
 
 RSpec.describe Sanitizer do
-  # TODO: Implement me!
+  subject { described_class }
+  describe ".filename" do
+    let(:filenames) { %w[dummy-name-for#my%file 123_dummy_file dummyname] }
+
+    let(:sanitized_filename) { %w[dummy_name_for_my_file 123_dummy_file dummyname] }
+
+    it "returns a proper filename" do
+      filenames.each_with_index do |filename, index|
+        expect(subject.filename(filename)).to eq(sanitized_filename[index])
+      end
+    end
+
+    context "when filename is blank" do
+      it "returns a Date string" do
+        allow(DateTime).to receive(:now).and_return("1632352627")
+
+        expect(subject.filename("")).to eq("1632352627")
+      end
+    end
+  end
 end
