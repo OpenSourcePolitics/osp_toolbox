93d1a328cd82544d98ce2b3911c6015c89a8cb11
Armand Fardeau <fardeauarmand@gmail.com>
2021-09-22 22:08:35 +0200

    Add pages spec

---
+++ spec/requests/pages_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "Pages", type: :request do
+  let(:user) { create(:user) }
+
+  describe "GET /" do
+    it "redirects to sign in" do
+      get ""
+
+      expect(response).to redirect_to("/users/sign_in")
+    end
+
+    context "when login in" do
+      before do
+        login_as(user)
+        get ""
+      end
+
+      it "render home template" do
+        expect(response).to render_template(:home)
+      end
+    end
+  end
+end
