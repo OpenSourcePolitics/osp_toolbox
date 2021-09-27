936b29de6b2e79e31962ea5d6103800c7d1b207f
Armand Fardeau <armandfardeau@users.noreply.github.com>
2021-09-28 00:09:18 +0200

    Add test for libs (#65)
    * Add test for libs
    
    * Fix vulnerability

--- spec/lib/document_input_file_serialize_spec.rb
+++ spec/lib/document_input_file_serialize_spec.rb
@@ -3,5 +3,17 @@
 require "rails_helper"
 
 RSpec.describe DocumentInputFilesSerializer do
-  # TODO: Implement me!
+  subject { described_class }
+  let!(:document) { create(:document) }
+  let!(:comments_input_file) { create(:input_file, :with_comment_file, document: document) }
+  let!(:proposals_input_file) { create(:input_file, document: document) }
+
+  describe ".serialize" do
+    it "serializes the input files" do
+      expect(subject.serialize(document.reload).keys).to eq(%w[Comments Proposals])
+      expect(subject.serialize(document.reload)["Comments"].length).to eq(25)
+      expect(subject.serialize(document.reload)["Proposals"].length).to eq(14)
+      expect(subject.serialize(document).class).to eq(Hash)
+    end
+  end
 end
--- spec/lib/xls_serializer_spec.rb
+++ spec/lib/xls_serializer_spec.rb
@@ -3,5 +3,15 @@
 require "rails_helper"
 
 RSpec.describe XlsSerializer do
-  # TODO: Implement me!
+  subject { described_class }
+
+  let(:preprocessing) { create(:processing, :with_xls_file) }
+  let(:data) { preprocessing.file }
+
+  describe ".serialize" do
+    it "serializes the data" do
+      expect(subject.serialize(data).keys).to eq((0..407).to_a.map(&:to_s))
+      expect(subject.serialize(data).class).to eq(Hash)
+    end
+  end
 end
