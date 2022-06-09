b7cb551d60748fb3063b41a16d0e2c517ba3a069
armandfardeau <fardeauarmand@gmail.com>
2022-06-09 22:17:20 +0200

    Add tests for archive

--- spec/models/archive_spec.rb
+++ spec/models/archive_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe Archive, type: :model do
   it "is valid" do
     expect(archive).to be_valid
   end
+
+  describe ".store_archive!" do
+    it "creates an attached file" do
+      # rubocop:disable Style/OpenStructUse
+      expect(archive.store_archive!(OpenStruct.new(response: "foo/bar"))).to eq(1)
+      # rubocop:enable Style/OpenStructUse
+    end
+  end
 end
