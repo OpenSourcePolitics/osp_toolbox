86bb2db5f9af2e260361c9dd2975ee853dbd93ee
armandfardeau <fardeauarmand@gmail.com>
2023-01-24 16:40:06 +0100

    use local disk for storage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
-  config.active_storage.service = :scaleway
+  config.active_storage.service = ENV.fetch('ACTIVE_STORAGE_SERVICE', "local").to_sym
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
